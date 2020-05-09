883893b09841c1023db8aa386a7aee9593614407
casey-minzi <cluvhope@gmail.com>
2020-05-09 18:45:57 +0100

    Updates

---
+++ precache-manifest.d5565f979cdc0f67f528bedd7d8341a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08450e2131958410b48c7466629bc10",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1e86c0b55a68c8360070",
+    "url": "/movie_app_2020/static/css/main.61fba7a3.chunk.css"
+  },
+  {
+    "revision": "1ad5fd741492895093c8",
+    "url": "/movie_app_2020/static/js/2.e6b2a0a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e6b2a0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e86c0b55a68c8360070",
+    "url": "/movie_app_2020/static/js/main.c6cf0d29.chunk.js"
+  },
+  {
+    "revision": "b2fbb1073761301bab7e",
+    "url": "/movie_app_2020/static/js/runtime-main.7d878977.js"
+  }
+]);
