cf2e56a1310f92a0c865ea1d4a2e9169519f0b06
casey-minzi <cluvhope@gmail.com>
2020-05-09 20:39:03 +0100

    Using React Redux Hook to store movie data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import Movie from '../components/Movie';
+import { useSelector, useDispatch } from 'react-redux';
 import './Home.css';
 
 function Home() {
 
-  const [movies, setMovies] = useState([]);
+  const movies = useSelector(state => state.movies);
+  const dispatch = useDispatch();
 
   const getMovies = async () => {
     const {
       data: {
         data: { movies }
       } } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    setMovies(movies);
+    dispatch({ type: 'UPLOAD_MOVIE_DATA', payload: movies })
   }
 
   useEffect(() => {
     getMovies();
-  }, [movies]);
+  });
 
   return (
     <div className="Home">
--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
 import { createStore } from 'redux';
 
-const reducer = () => {
-    console.log('hello');
+const initialState = {movies: []};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'UPLOAD_MOVIE_DATA' :
+            return {movies: action.payload};
+    default :
+        return state;
+    }
 }
+
 
 const store = createStore(reducer);
 
